7a5e0fb8b27dabb547e38d4e8e1434df78584ec6
Abu Bakar Panhwar <abubakar.ahmed267@gmail.com>
2025-08-20 20:44:16 +0500

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,33 @@
+# Last updated: 20/08/2025, 20:44:12
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False  
+
+        temp = x
+        reverse_num = 0
+        while temp !=0:
+            last_digit = temp%10
+            temp = temp // 10
+            reverse_num = reverse_num*10 + last_digit
+
+        print(reverse_num)
+
+        if reverse_num == x :
+            return True
+        else :
+            return False
+
+
+
+        
+        # if x<0 or x!=0 and x%10 == 0:     #should not end with 0     
+        #     return False
+
+        # half = 0
+
+        # while half<x:
+        #     half = (half*10) + (x%10)
+        #     x = x//10
+
+        # return x == half or x == half//10
